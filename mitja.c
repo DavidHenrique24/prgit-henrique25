31edcd7be988d1009f811bd7165814cf29bb4a49
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2024-09-24 17:18:09 +0200

    prgit-henrique25 v2

--- mitja.c
+++ mitja.c
@@ -13,7 +13,7 @@ int main() {
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("Henrique 25 - Vols continuar (s/n): ");
-		scanf(" %c",&consulta); 
+		scanf("%s",&consulta);
 	}
 	mitja=suma/numero_repeticions;
 	printf("Henrique25 - La mitja és: %.2f\n",mitja);	
